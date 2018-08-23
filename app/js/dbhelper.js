8989de54e855116ac4f42736f1be41c61e3e995f
Julio <juliointelek@hotmail.com>
2018-08-23 13:47:10 +0200

    repair error with images

--- app/js/dbhelper.js
+++ app/js/dbhelper.js
@@ -194,7 +194,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`+`.jpg`);
   }
 
   /**
