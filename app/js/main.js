4a1d87b471f406d67dd2d3b4e0abe9c8a75f1597
Julio <juliointelek@hotmail.com>
2018-08-26 19:39:17 +0200

    repaired dbhelper with indexeddb without dexie

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,25 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+//activamos el listener para instalar la app
+window.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
+
+  // e.userChoice will return a Promise. 
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
+
 
 
 let restaurants,
@@ -19,6 +38,7 @@ let restaurants,
   cuisines
 var newMap
 var markers = []
+var db
 
 
 /**
